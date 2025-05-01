16107e23a44bc576a3a4f4fb8874aeea084adc23
LivingIchor <livingichor@gmail.com>
2025-04-30 20:36:29 -0400

    Initial commit

---
+++ build/CMakeFiles/watch-chezmoi-dirs.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/watch-chezmoi-dirs.dir/link.d"
+  "CMakeFiles/watch-chezmoi-dirs.dir/main.c.o"
+  "CMakeFiles/watch-chezmoi-dirs.dir/main.c.o.d"
+  "watch-chezmoi-dirs"
+  "watch-chezmoi-dirs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/watch-chezmoi-dirs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/watch-chezmoi-dirs.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for watch-chezmoi-dirs.
+# This may be replaced when dependencies are built.
